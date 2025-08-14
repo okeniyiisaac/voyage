fab5363aff074f456433c2730f28f8c01eb9f42e
iluyemio <iluyemio@gmail.com>
2025-08-14 14:31:25 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <section>
+      <h1 className='text-6xl text-center'>This is our Hackathon project</h1>
+    </section>
+  )
+}
+
+export default App
