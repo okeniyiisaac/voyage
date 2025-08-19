58dd946155b6603926d48934259a577386927dd6
kzeebaba <aderonmu.kazeem@yahoo.com>
2025-08-19 17:28:07 +0100

    updated activities and Activitiesdetail page

--- src/pages/Activities.jsx
+++ src/pages/Activities.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import { BsChevronDoubleRight } from "react-icons/bs";
 
 function Activities() {
   return (
@@ -9,12 +9,12 @@ function Activities() {
                 <div className='bg-slate-600 h-96'>
                     <div className='space-y-9 py-40 px-[40%]'>
                         <h2 className='text-white text-6xl text'>Activities</h2>
-                        <ul className='px-4 m-8 text-1xl text-white flex space-x-2 border w-[160px] rounded-full p-2'>
+                        <ul className='px-4 m-8 text-1xl text-white flex  justify-center space-x-2 border w-[166px] rounded-full p-2'>
                           <li>
                             Home
                           </li>
                           <li>
-
+                            <BsChevronDoubleRight  className="my-1"/>
                           </li>
                           <li>
                             Activities
@@ -25,58 +25,105 @@ function Activities() {
             </div>
         </section>
         <section className='my-20'>
+            <div className='lg:flex justify-center gap-8'>
+                                   
+                    <div>
+                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(5 Trips) </div>
+                        </div>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">California</h4> 
+                            </a>
+                        </div>
+                    </div> 
+                    <div>
+                        <div className=" bg-[url('./assets/thai-rw1.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(2 Trips) </div>
+                        </div>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">Thailand</h4> 
+                            </a>
+                        </div>
+                    </div> 
+                    
+                     <div>
+                        <div className=" bg-[url('./assets/india-rw1.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(3 Trips) </div>
+                        </div>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">India</h4> 
+                            </a>
+                        </div>
+                    </div>                   
+                    
+            </div>
             <div className='lg:flex justify-center gap-7 '>
                                    
                     <div>
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72 py-7 px-4">                                                    
-                            <div className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(5 Trips)</div>
+                        <div className=" bg-[url('./assets/sugar-rw2.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(6 Trips) </div>
                         </div>
                         <div className="mb-7 mt-4">
-                            <h4>California</h4>
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">Sugarland</h4> 
+                            </a>
                         </div>
-                    </div>
+                    </div> 
                     <div>
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72 py-7 px-4">                                                    
-                            <div className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(5 Trips)</div>
+                        <div className=" bg-[url('./assets/cal-rw2.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(7 Trips) </div>
                         </div>
                         <div className="mb-7 mt-4">
-                            <h4>California</h4>
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">California</h4> 
+                            </a>
                         </div>
-                    </div>
+                    </div> 
                      <div>
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72 py-7 px-4">                                                    
-                            <div className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(5 Trips)</div>
+                        <div className=" bg-[url('./assets/suga-rw2last.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(8 Trips) </div>
                         </div>
                         <div className="mb-7 mt-4">
-                            <h4>California</h4>
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">Sugarland</h4> 
+                            </a>
                         </div>
                     </div>                   
                 
             </div>
-        <div className='lg:flex  justify-center gap-7'>
+            <div className='lg:flex justify-center gap-7 '>
                                    
                     <div>
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72">                                                    
-                            <div>(5 Trips)</div>
+                        <div className=" bg-[url('./assets/india-rw3.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(6 Trips) </div>
                         </div>
-                        <div>
-                            <h4>California</h4>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">India</h4> 
+                            </a>
                         </div>
-                    </div>
-                    <div> 
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72">                                                    
-                            <div>(5 Trips)</div>
+                    </div> 
+                    <div>
+                        <div className=" bg-[url('./assets/cal-rw3.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(3 Trips) </div>
                         </div>
-                        <div>
-                            <h4>California</h4>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">California</h4> 
+                            </a>
                         </div>
-                    </div>
+                    </div> 
                      <div>
-                        <div className=" bg-[url('./assets/cal.jpg')] rounded-xl w-80 h-72">                                                    
-                            <div>(5 Trips)</div>
+                        <div className=" bg-[url('./assets/thai-rw3.jpg')] rounded-xl w-80 h-96 py-7 px-4">                                                    
+                            <div  className='bg-[#0dcaf0] text-white w-fit px-2 rounded-xl '>(5 Trips) </div>
                         </div>
-                        <div>
-                            <h4>California</h4>
+                        <div className="mb-7 mt-4">
+                            <a href="#" rel="noopener noreferrer">
+                            <h4 className=" cursor-pointer hover:text-[#0dcaf0] text-[20px] font-bold">Thailand</h4> 
+                            </a>
                         </div>
                     </div>                    
                 
